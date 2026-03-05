42cca3e4667e04dd78e2be66822a3f29b4e7af9d
gallys <gallys>
2026-03-05 17:38:50 +0700

    Update

--- app/routers/reviews.py
+++ app/routers/reviews.py
@@ -1,17 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select, update
 from sqlalchemy.sql import func
-from sqlalchemy.orm import Session
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models.reviews import Review as ReviewModel
 from app.models.products import Product as ProductModel
-from app.models.categories import Category as CategoryModel
-from app.models.users import User as UserModel
-from app.schemas import Product as ProductSchema, ProductCreate
 from app.schemas import Review as ReviewSchema, ReviewCreate
-from app.db_depends import get_db, get_async_db
-from app.auth import get_current_buyer, get_current_admin, get_current_user
+from app.db_depends import get_async_db
+from app.auth import get_current_buyer, get_current_user
 
 router = APIRouter(
     tags=["reviews"]
